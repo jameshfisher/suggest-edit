9e766a9b611ff2b38263d13b25eaeddfd7729a5a
Jim Fisher <jameshfisher@gmail.com>
2018-06-22 23:20:04 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Suggest edit
+
+```
+<script src="https://unpkg.com/diff@3.5.0/dist/diff.js"></script>
+<script src="https://rawgit.com/jameshfisher/suggest-edit/master/suggest-edit.js"></script>
+<button id="suggest-edit">Suggest edit</button>
+```
---
+++ suggest-edit.js
@@ -0,0 +1,25 @@
+document.onload = () => {
+  const btnEl = document.getElementById("suggestedit");
+  let beforeEdit = null;
+  btnEl.onclick = (e) => {
+    e.preventDefault();
+    if (beforeEdit === null) {
+      btnEl.classList.add("editing");
+      document.body.contentEditable = true;
+      beforeEdit = document.body.innerHTML;
+    } else {
+      afterEdit = document.body.innerHTML;
+      document.body.contentEditable = false;
+      btnEl.classList.remove("editing");
+      const patch = JsDiff.createPatch(window.location, beforeEdit, afterEdit, "", "");
+      beforeEdit = null;
+      fetch("https://hooks.zapier.com/hooks/catch/2257982/wddkks/", {
+        method: "POST",
+        body: JSON.stringify({
+          url: window.location,
+          diff: patch
+        })
+      });
+    }
+  };
+}
